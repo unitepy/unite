da2a525bf1ce4b76d01c6e49dc72d73ea535e088
Siewdass Sf <siewdass@gmail.com>
2021-03-01 10:39:29 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <Python.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main( int argc, char** argv ) {
+
+	if ( argc == 2 ) {
+
+		std::string code;
+
+		std::string read;
+
+		std::ifstream file;
+		file.open( argv[ 1 ] );
+
+		if ( file.is_open( ) ) {
+
+			code.append( "from sys import path as dontknow\ndontknow.append('python-modules')\n" );
+
+			for ( std::string line; getline( file, line ); ) {
+				if ( line == "#!/usr/bin/env python3" ) {
+					code.insert( 0, "#!/usr/bin/env python3\n" );
+				} else {
+					code.append( line + "\n" );
+				}
+			}
+
+			file.close( );
+
+		} else {
+			std::cout << "Unable to open the file!" << std::endl;
+		}
+
+		Py_Initialize( );
+		PyRun_SimpleString( code.c_str( ) );
+		Py_Finalize( );
+
+	}
+
+	return 0;
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#from daemonize import Daemonize
+
+test = 'Omg'
+
+print( test ) 
